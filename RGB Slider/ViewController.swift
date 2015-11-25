0d59d8ffc819ce59f9c48cd419d108248e3ed405
case-dubs <casey594@gmail.com>
2015-11-24 17:31:01 -0800

    updated code to include color box

--- RGB Slider/ViewController.swift
+++ RGB Slider/ViewController.swift
@@ -9,9 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: Variables
+    
+    @IBOutlet weak var redSelection: UISlider!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenSelection: UISlider!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueSelection: UISlider!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var colorOutputBox: UIView!
+    
+    
+    //MARK: Functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(red: CGFloat(186)/255, green: CGFloat(221)/255, blue: CGFloat(188)/255, alpha: 1.0)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +32,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateColorLabels(red: Float, blue: Float, green: Float){
+        redLabel.text = String(Int(red))
+        blueLabel.text = String(Int(blue))
+        greenLabel.text = String(Int(green))
+    }
+    @IBAction func updateBackgroundColor(sender: AnyObject) {
+        let red = redSelection.value
+        let green = greenSelection.value
+        let blue = blueSelection.value
+        colorOutputBox.backgroundColor = UIColor(red: CGFloat(red)/255, green: CGFloat(green)/255, blue: CGFloat(blue)/255, alpha: 1.0)
+        updateColorLabels(red, blue: blue, green: green)
+    }
+    
 
 
 }
